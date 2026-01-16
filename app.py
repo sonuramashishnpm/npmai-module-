b71c11055fa344b2f5ae9b3ee10811848a791bd3
Sonu Kumar <sonuramashishnpm@gmail.com>
2026-01-16 19:27:09 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+import os
+
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def render_index():
+  return render_template("index.html")
+  
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
